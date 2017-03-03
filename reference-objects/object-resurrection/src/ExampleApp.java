b8de3f54a2c119441b47f679c01ff04f2fb8e566
vundicind <vundicind@gmail.com>
2017-03-03 22:06:58 +0200

    Add project 'object-resurrection' files

---
+++ reference-objects/object-resurrection/src/ExampleApp.java
@@ -0,0 +1,22 @@
+/**
+ * Created by vundicind on 3/3/17.
+ */
+public class ExampleApp {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+
+        foo = null;
+
+        for (int i=0; i < 1; i++) {
+            System.gc();
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        Foo.instance.saySomething();
+    }
+}
