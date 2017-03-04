d908ed4f904727dd2ad89d6976fe2b22ae18834d
vundicind <vundicind@gmail.com>
2017-03-04 09:46:40 +0200

    Add project 'out-of-memory' files

---
+++ reference-objects/out-of-memory/src/Foo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by vundicind on 3/3/17.
+ */
+public class Foo {
+    private byte[] array;
+
+    public Foo() {
+        System.out.println("The object is about to be created");
+        array = new byte[(int)Runtime.getRuntime().totalMemory() / 2 + 1];
+    }
+}
